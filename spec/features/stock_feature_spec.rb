0901798de4f68d35962687afcfaa8b1025d9d30a
Richard Bates <richo225@gmail.com>
2016-08-30 22:31:03 +0100

    User can update stock using checklist. No form yet

--- spec/features/stock_feature_spec.rb
+++ spec/features/stock_feature_spec.rb
@@ -9,4 +9,15 @@ feature "updating stock" do
       expect(current_path).to eq "/"
     end
   end
+
+  context "range of goods" do
+    scenario "all range of goods are listed" do
+      Product.create(name:'Apple')
+      Product.create(name:'Potato')
+      sign_up
+      click_link "Add stock"
+      expect(page).to have_content("Apple")
+      expect(page).to have_content("Potato")
+    end
+  end
 end
