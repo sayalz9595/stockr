34657d5f65197d847299ce812e15d295891422c8
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-08-30 15:26:41 +0100

    product can be editted and deleted

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,14 +9,37 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    find_product
+  end
+
+  def update
+    find_product
+    @product.update(product_params)
+    redirect_to products_path
+  end
+
+  def destroy
+    find_product
+    @product.destroy
     redirect_to products_path
   end
 
   private
+
   def product_params
     params.require(:product).permit(:name)
   end
 
-
+  def find_product
+    @product = Product.find(params[:id])
+  end
 
 end
--- spec/features/product_spec.rb
+++ spec/features/product_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 feature 'product' do
-  scenario'a new product can be added' do
+  scenario 'a new product can be added' do
     visit '/products'
     click_link 'Add a product'
     fill_in 'Name', with: "Apple"
     click_button 'Create Product'
     expect(page).to have_content("Apple")
     expect(Product.count).to eq 1
+  end
+  scenario 'a product can be editted' do
+    Product.create(name:'Appl')
+    visit '/products'
+    click_link 'Edit Appl'
+    fill_in 'Name', with: "Apple"
+    click_button 'Update Product'
+    expect(page).to have_content("Apple")
+  end
+
+  scenario 'a product can be deleted' do
+    Product.create(name:'Apple')
+    visit '/products'
+    click_link 'Delete Apple'
+    expect(page).not_to have_content("Apple")
   end
 end
