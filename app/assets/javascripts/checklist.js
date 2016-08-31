16f2a16e99f9d26a9c50be5cec0d7e35f1c6ed61
Richard Bates <richo225@gmail.com>
2016-08-31 22:38:27 +0100

    Add jquery to select all stock checkboxes

---
+++ app/assets/javascripts/checklist.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $("#selectAll").click(function(){
+    if ($(this).is(":checked")) {
+      $(":checkbox").each(function(){
+        this.checked = true;
+      });
+    }
+    else {
+      $(":checkbox").each(function(){
+        this.checked = false;
+      });
+    }
+  });
+});
+
+// OR
+
+// $(document).ready(function(){
+//   $("#selectAll").click(function(){
+//     if ($(this).is(":checked")) {
+//       $("input[name*='stock']").each(function(){
+//         this.checked = true;
+//       });
+//     }
+//     else {
+//       $("input[name*='stock']").each(function(){
+//         this.checked = false;
+//       });
+//     }
+//   });
+// });
