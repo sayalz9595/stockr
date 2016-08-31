1c6cf2a11b99e7e78e7f2149311988527c4ee6e0
Richard Bates <richo225@gmail.com>
2016-08-31 19:24:00 +0100

    Add product ref to stock. Tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831182028) do
+ActiveRecord::Schema.define(version: 20160831182303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,10 @@ ActiveRecord::Schema.define(version: 20160831182028) do
   create_table "stocks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "product_id"
   end
+
+  add_index "stocks", ["product_id"], name: "index_stocks_on_product_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -56,4 +59,5 @@ ActiveRecord::Schema.define(version: 20160831182028) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "customers", "users"
+  add_foreign_key "stocks", "products"
 end
