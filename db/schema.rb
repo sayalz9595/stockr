322cefe77c7e4e7ab1c3268029d9d2b0b0a7122b
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-09-02 10:25:42 +0100

    fix stock create, refactor stock controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901150612) do
+ActiveRecord::Schema.define(version: 20160902090242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 20160901150612) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "stock_id"
   end
 
+  add_index "products", ["stock_id"], name: "index_products_on_stock_id", using: :btree
   add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "stocklists", force: :cascade do |t|
@@ -72,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160901150612) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "customers", "users"
+  add_foreign_key "products", "stocks"
   add_foreign_key "products", "users"
   add_foreign_key "stocklists", "products"
   add_foreign_key "stocks", "products"
